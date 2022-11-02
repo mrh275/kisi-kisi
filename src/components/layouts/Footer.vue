8774527d1a573b150150adec962d4af3fd19a7e2
Muhamad Ramdani Hidayatullah <muhamadramdani.h275@gmail.com>
2022-11-02 13:51:09 +0700

    added footer component

---
+++ src/components/layouts/Footer.vue
@@ -0,0 +1,16 @@
+<script>
+export default {
+  data() {
+    return {
+      dateNow: new Date().getFullYear(),
+    };
+  },
+};
+</script>
+
+<template>
+  <footer class="flex flex-col py-6 text-center text-white bg-gray-700 footer">
+    <span class="copyright">Copyright {{ dateNow }} ©️ SMAN 1 Rawamerta. All Right Reserved.</span>
+    <span>Made by <a href="https://muhamadramdani.my.id" target="_blank" class="hover:text-[#0099ff] border-b border-dotted">dNezast</a>.</span>
+  </footer>
+</template>
