e2944923af52769bdbd429b55c71eb3f0ba21bd8
Muhamad Ramdani Hidayatullah <muhamadramdani.h275@gmail.com>
2022-11-02 13:52:12 +0700

    added section for greetings

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,7 +1,13 @@
 <script></script>
 
 <template>
-  <main>
-    <h1>Home</h1>
+  <main class="px-6 py-8 text-white bg-gray-800">
+    <div class="greetings-wrapper">
+      <h1 class="text-4xl font-bold">Selamat Datang!</h1>
+      <h1 class="text-3xl font-bold">Selamat Belajar sebelum PAS!</h1>
+    </div>
+    <div class="my-8 splash-img-wrapper">
+      <img src="../assets/study.svg" alt="Studying" />
+    </div>
   </main>
 </template>
