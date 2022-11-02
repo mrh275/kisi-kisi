3bb2d777542e3a38ff0c2fc07b8c8006355b7276
Muhamad Ramdani Hidayatullah <muhamadramdani.h275@gmail.com>
2022-11-02 13:52:23 +0700

    render footer component

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,13 @@
 <script setup>
 import { RouterLink, RouterView } from "vue-router";
 import HeaderVue from "./components/layouts/Header.vue";
+import FooterVue from "./components/layouts/Footer.vue";
 </script>
 
 <template>
   <header-vue></header-vue>
   <RouterView />
+  <footer-vue></footer-vue>
 </template>
 
 <style scoped></style>
