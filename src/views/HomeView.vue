ea616c8fba7a9f3f091c73908a9345eb322deec7
Muhamad Ramdani Hidayatullah <muhamadramdani.h275@gmail.com>
2022-11-02 14:05:04 +0700

    added years grade

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -5,13 +5,17 @@
     <div class="greetings-wrapper">
       <h1 class="text-4xl font-bold">Selamat Datang!</h1>
       <h1 class="text-3xl font-bold">Selamat Belajar sebelum PAS!</h1>
+      <h1 class="text-2xl font-bold">Tahun Pelajaran 2022/2023</h1>
     </div>
     <div class="my-8 splash-img-wrapper">
       <img src="../assets/study.svg" alt="Studying" />
     </div>
     <hr class="my-8 opacity-60" />
     <div class="text-center list-kisi">
-      <h3 class="mb-6 text-xl font-semibold text-center list-kisi-title">Daftar Kisi-Kisi Mata Pelajaran</h3>
+      <h3 class="mb-6 text-xl font-semibold text-center list-kisi-title">
+        Daftar Kisi-Kisi <br />
+        Mata Pelajaran
+      </h3>
       <p class="text-justify list-kisi-desc">Untuk melihat daftar kisi-kisi mata pelajaran apa saja yang sudah tersedia, silahkan klik tombol dibawah ini!</p>
       <a href="/daftar-kisi-kisi" class="inline-block my-6 bg-[#0099ff] rounded-lg px-4 py-3 list-kisi-cta btn btn-primary">Daftar Kisi-kisi</a>
     </div>
