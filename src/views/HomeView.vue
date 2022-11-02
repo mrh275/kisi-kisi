b79514f46879bdb7c84e6a397830c2916381b054
Muhamad Ramdani Hidayatullah <muhamadramdani.h275@gmail.com>
2022-11-02 14:01:42 +0700

    added kisi-kisi section with cta

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -9,5 +9,11 @@
     <div class="my-8 splash-img-wrapper">
       <img src="../assets/study.svg" alt="Studying" />
     </div>
+    <hr class="my-8 opacity-60" />
+    <div class="text-center list-kisi">
+      <h3 class="mb-6 text-xl font-semibold text-center list-kisi-title">Daftar Kisi-Kisi Mata Pelajaran</h3>
+      <p class="text-justify list-kisi-desc">Untuk melihat daftar kisi-kisi mata pelajaran apa saja yang sudah tersedia, silahkan klik tombol dibawah ini!</p>
+      <a href="/daftar-kisi-kisi" class="inline-block my-6 bg-[#0099ff] rounded-lg px-4 py-3 list-kisi-cta btn btn-primary">Daftar Kisi-kisi</a>
+    </div>
   </main>
 </template>
