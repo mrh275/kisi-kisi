745ec7354992969e5ce47223174bb9bcbcd5730a
Muhamad Ramdani Hidayatullah <muhamadramdani.h275@gmail.com>
2022-11-05 22:16:02 +0700

    remove margin bottom on desktop breakpoints

--- src/components/layouts/Footer.vue
+++ src/components/layouts/Footer.vue
@@ -9,7 +9,7 @@ export default {
 </script>
 
 <template>
-  <footer class="flex flex-col px-6 py-6 mb-20 text-center text-white bg-gray-700 footer">
+  <footer class="flex flex-col px-6 py-6 mb-20 text-center text-white bg-gray-700 xl:mb-0 footer">
     <span class="copyright">Copyright {{ dateNow }} ©️ SMAN 1 Rawamerta. All Right Reserved.</span>
     <span>Made by <a href="https://muhamadramdani.my.id" target="_blank" class="hover:text-[#0099ff] border-b border-dotted">dNezast</a>.</span>
   </footer>
