58d3465e3efe053318e0ac2a3dfe5afaaaadcf72
Muhamad Ramdani Hidayatullah <muhamadramdani.h275@gmail.com>
2022-11-06 23:08:30 +0700

    added header content

--- src/views/DaftarKisiKisi.vue
+++ src/views/DaftarKisiKisi.vue
@@ -1,15 +1,18 @@
-<template>
-  <div class="about">
-    <h1>This is an about page</h1>
-  </div>
-</template>
+<script>
+export default {};
+</script>
 
-<style>
-@media (min-width: 1024px) {
-  .about {
-    min-height: 100vh;
-    display: flex;
-    align-items: center;
-  }
-}
-</style>
+<template>
+  <main class="px-6 py-8 text-white bg-gray-800">
+    <div class="mb-6 kisi-wrapper">
+      <h2 class="text-xl font-semibold text-center kisi-title">
+        Daftar Kisi-Kisi <br />
+        Mata Pelajaran <br />
+        Penilaian Akhir Semester 1 (PAS) <br />
+        Tahun Pelajaran 2022/2023
+      </h2>
+    </div>
+
+    <!-- Table Kisi -->
+  </main>
+</template>
