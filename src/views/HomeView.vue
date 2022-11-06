b926346a41e87b3a689547d0a111a34afe67d999
Muhamad Ramdani Hidayatullah <muhamadramdani.h275@gmail.com>
2022-11-06 20:12:02 +0700

    added some properties on desktop breakpoints

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -2,22 +2,22 @@
 
 <template>
   <main class="px-6 py-8 text-white bg-gray-800">
-    <div class="first-wrapper xl:flex xl:justify-between">
-      <div class="greetings-wrapper xl:w-1/2">
-        <h1 class="text-4xl font-bold">Selamat Datang!</h1>
-        <h1 class="text-3xl font-bold">Selamat Belajar sebelum PAS!</h1>
-        <h1 class="text-2xl font-bold">Tahun Pelajaran 2022/2023</h1>
+    <div class="first-wrapper xl:flex xl:justify-between xl:mb-6">
+      <div class="py-12 greetings-wrapper xl:w-1/2">
+        <h1 class="text-4xl font-bold xl:text-5xl">Selamat Datang!</h1>
+        <h1 class="text-3xl font-bold xl:text-4xl">Selamat Belajar sebelum PAS!</h1>
+        <h1 class="text-2xl font-bold xl:text-3xl">Tahun Pelajaran 2022/2023</h1>
       </div>
-      <div class="my-8 splash-img-wrapper xl:w-1/2">
-        <img src="../assets/study.svg" alt="Studying" />
+      <div class="my-8 splash-img-wrapper xl:w-1/2 xl:flex xl:justify-center xl:items-center">
+        <img src="../assets/study.svg" alt="Studying" class="xl:w-3/4" />
       </div>
     </div>
-    <div class="text-center list-kisi">
-      <h3 class="mb-6 text-xl font-semibold text-center list-kisi-title">
+    <div class="text-center second-wrapper list-kisi">
+      <h3 class="mb-6 text-xl font-semibold text-center xl:text-3xl list-kisi-title">
         Daftar Kisi-Kisi <br />
         Mata Pelajaran
       </h3>
-      <p class="text-justify list-kisi-desc">Untuk melihat daftar kisi-kisi mata pelajaran apa saja yang sudah tersedia, silahkan klik tombol dibawah ini!</p>
+      <p class="text-justify xl:text-center xl:text-xl list-kisi-desc">Untuk melihat daftar kisi-kisi mata pelajaran apa saja yang sudah tersedia, silahkan klik tombol dibawah ini!</p>
       <a href="/daftar-kisi-kisi" class="inline-block my-6 bg-[#0099ff] rounded-lg px-4 py-3 list-kisi-cta btn btn-primary">Daftar Kisi-kisi</a>
     </div>
   </main>
