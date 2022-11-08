91b53d5d1eff9b2491b01239ad89d01cf1a774ad
Muhamad Ramdani Hidayatullah <muhamadramdani.h275@gmail.com>
2022-11-08 19:13:23 +0700

    removed vue3-easy-data-tables

--- src/main.js
+++ src/main.js
@@ -6,8 +6,6 @@ import { faHome, faBook, faBars, faDownload } from "@fortawesome/free-solid-svg-
 import { FontAwesomeIcon } from "@fortawesome/vue-fontawesome";
 import App from "./App.vue";
 import router from "./router";
-import Vue3EasyDataTable from "vue3-easy-data-table";
-import "vue3-easy-data-table/dist/style.css";
 
 import "./assets/main.css";
 
@@ -17,6 +15,5 @@ const app = createApp(App);
 app.use(createPinia());
 app.use(router);
 app.component("fa", FontAwesomeIcon);
-app.component("EasyDataTable", Vue3EasyDataTable);
 
 app.mount("#app");
