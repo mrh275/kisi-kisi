6cfd2f19e84475ae594f6421902a4800e3df12fd
Muhamad Ramdani Hidayatullah <muhamadramdani.h275@gmail.com>
2022-11-08 20:03:14 +0700

    remount datatable component

--- src/main.js
+++ src/main.js
@@ -6,8 +6,9 @@ import { faHome, faBook, faBars, faDownload } from "@fortawesome/free-solid-svg-
 import { FontAwesomeIcon } from "@fortawesome/vue-fontawesome";
 import App from "./App.vue";
 import router from "./router";
-
 import "./assets/main.css";
+import Vue3EasyDataTable from "vue3-easy-data-table";
+import "vue3-easy-data-table/dist/style.css";
 
 library.add(faHome, faBook, faBars, faDownload);
 const app = createApp(App);
@@ -15,5 +16,6 @@ const app = createApp(App);
 app.use(createPinia());
 app.use(router);
 app.component("fa", FontAwesomeIcon);
+app.component("EasyDataTable", Vue3EasyDataTable);
 
 app.mount("#app");
