ebffe8166e3020a774878f4098c26c18d34f6f98
Muhamad Ramdani Hidayatullah <muhamadramdani.h275@gmail.com>
2022-11-09 20:49:45 +0700

    added login icon

--- src/main.js
+++ src/main.js
@@ -2,7 +2,7 @@ import { createApp } from "vue";
 import { createPinia } from "pinia";
 /* import font awesome icon component */
 import { library } from "@fortawesome/fontawesome-svg-core";
-import { faHome, faBook, faBars, faDownload } from "@fortawesome/free-solid-svg-icons";
+import { faHome, faBook, faBars, faDownload, faSignIn } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/vue-fontawesome";
 import App from "./App.vue";
 import router from "./router";
@@ -10,7 +10,7 @@ import "./assets/main.css";
 import Vue3EasyDataTable from "vue3-easy-data-table";
 import "vue3-easy-data-table/dist/style.css";
 
-library.add(faHome, faBook, faBars, faDownload);
+library.add(faHome, faBook, faBars, faDownload, faSignIn);
 const app = createApp(App);
 
 app.use(createPinia());
