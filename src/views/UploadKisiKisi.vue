ac07026fa5e5f3d05cc3f77f6b49193b0ef6f408
Muhamad Ramdani Hidayatullah <muhamadramdani.h275@gmail.com>
2022-11-14 19:39:56 +0700

    added event to upload files using vue3-dropzone

--- src/views/UploadKisiKisi.vue
+++ src/views/UploadKisiKisi.vue
@@ -1,3 +1,41 @@
-<script setup></script>
+<script>
+import { useDropzone } from "vue3-dropzone";
 
-<template></template>
+export default {
+  name: "UploadKisiKisi",
+  setup() {
+    const url = "http://127.0.0.1:8000/api/uploadFiles";
+    const saveFilesTmp = (files) => {
+      const formData = new FormData();
+      for (let i = 0; i < files.length; i++) {
+        formData.append("tmpFiles[]", files[i]);
+      }
+      console.log(formData.getAll("tmpFiles[]"));
+    };
+
+    function onDrop(acceptFiles, rejectReason) {
+      saveFilesTmp(acceptFiles);
+      //   console.log(rejectReason);
+    }
+
+    const { getRootProps, getInputProps, ...rest } = useDropzone({ onDrop });
+
+    return {
+      getRootProps,
+      getInputProps,
+      ...rest,
+    };
+  },
+};
+</script>
+
+<template>
+  <div>
+    <div v-bind="getRootProps()">
+      <input v-bind="getInputProps()" />
+      <p v-if="isDragActive">Drop the files here...</p>
+      <p v-else>Drag and Drop some files here, or click to select files</p>
+    </div>
+    <button @click="open">Open</button>
+  </div>
+</template>
