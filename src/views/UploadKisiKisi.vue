00489f8169c8b88955bb359e5a0ed208a59bdb63
Muhamad Ramdani Hidayatullah <muhamadramdani.h275@gmail.com>
2022-11-16 11:37:54 +0700

    added http request to upload files using axios

--- src/views/UploadKisiKisi.vue
+++ src/views/UploadKisiKisi.vue
@@ -1,16 +1,29 @@
 <script>
 import { useDropzone } from "vue3-dropzone";
+import axios from "axios";
 
 export default {
   name: "UploadKisiKisi",
   setup() {
-    const url = "http://127.0.0.1:8000/api/uploadFiles";
+    const url = "/api/upload-kisi-kisi";
     const saveFilesTmp = (files) => {
       const formData = new FormData();
       for (let i = 0; i < files.length; i++) {
         formData.append("tmpFiles[]", files[i]);
       }
       console.log(formData.getAll("tmpFiles[]"));
+      axios
+        .post(url, formData, {
+          headers: {
+            "Content-Type": "multipart/form-data",
+          },
+        })
+        .then((response) => {
+          console.log(response.data);
+        })
+        .catch((err) => {
+          console.log(err);
+        });
     };
 
     function onDrop(acceptFiles, rejectReason) {
@@ -30,12 +43,21 @@ export default {
 </script>
 
 <template>
-  <div>
-    <div v-bind="getRootProps()">
-      <input v-bind="getInputProps()" />
-      <p v-if="isDragActive">Drop the files here...</p>
-      <p v-else>Drag and Drop some files here, or click to select files</p>
-    </div>
-    <button @click="open">Open</button>
+  <div class="flex items-center justify-center w-full">
+    <label
+      v-bind="getRootProps()"
+      for="dropzone-file"
+      class="flex flex-col items-center justify-center w-full h-64 border-2 border-gray-300 border-dashed rounded-lg cursor-pointer bg-gray-50 dark:hover:bg-bray-800 dark:bg-gray-700 hover:bg-gray-100 dark:border-gray-600 dark:hover:border-gray-500 dark:hover:bg-gray-600"
+    >
+      <div class="flex flex-col items-center justify-center pt-5 pb-6">
+        <svg aria-hidden="true" class="w-10 h-10 mb-3 text-gray-400" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
+          <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M7 16a4 4 0 01-.88-7.903A5 5 0 1115.9 6L16 6a5 5 0 011 9.9M15 13l-3-3m0 0l-3 3m3-3v12"></path>
+        </svg>
+        <p v-if="isDragActive" class="mb-2 text-sm text-gray-500 dark:text-gray-400"><span class="font-semibold">Drop the files here...</span></p>
+        <p v-else class="mb-2 text-sm text-gray-500 dark:text-gray-400"><span class="font-semibold">Click to upload</span> or drag and drop</p>
+        <p class="text-xs text-gray-500 dark:text-gray-400">SVG, PNG, JPG or GIF (MAX. 800x400px)</p>
+      </div>
+      <input id="dropzone-file" type="file" class="hidden" v-bind="getInputProps()" />
+    </label>
   </div>
 </template>
