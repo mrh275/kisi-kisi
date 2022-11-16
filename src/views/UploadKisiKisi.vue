c942c613fb23d7b48ef35ded5ee5d67715b61535
Muhamad Ramdani Hidayatullah <muhamadramdani.h275@gmail.com>
2022-11-16 21:48:43 +0700

    added input validation

--- src/views/UploadKisiKisi.vue
+++ src/views/UploadKisiKisi.vue
@@ -1,5 +1,6 @@
 <script>
 import axios from "axios";
+import { createToaster } from "@meforma/vue-toaster";
 
 export default {
   name: "UploadKisiKisi",
@@ -9,12 +10,31 @@ export default {
       kelas: null,
       jurusan: null,
       selectedFiles: null,
+      mapelError: null,
+      kelasError: null,
+      jurusanError: null,
+      fileErrors: null,
     };
   },
   methods: {
     sendMapel(event) {
       event.preventDefault();
-      const data = {
+      if (this.mapel == null) {
+        this.mapelError = "Nama mata pelajaran wajib diisi!";
+      } else {
+        this.mapelError = null;
+      }
+      if (this.kelas == null) {
+        this.kelasError = "Kelas wajib diisi!";
+      } else {
+        this.kelasError = null;
+      }
+      if (this.jurusan == null) {
+        this.jurusanError = "Jurusan wajib diisi!";
+      } else {
+        this.jurusanError = null;
+      }
+      data = {
         mapel: this.mapel,
         kelas: this.kelas + " " + this.jurusan,
         status: 1,
@@ -22,6 +42,7 @@ export default {
         fileMapel: [this.selectedFiles],
       };
       const url = "/api/upload-kisi-kisi";
+
       axios
         .post(url, data, {
           headers: {
@@ -29,14 +50,33 @@ export default {
           },
         })
         .then((response) => {
-          console.log(response.data);
+          const toaster = createToaster({
+            position: "top-right",
+            duration: 3000,
+            dismissible: true,
+          });
+          toaster.success(response.data);
+          this.mapel = "";
+          this.kelas = "";
+          this.jurusan = "";
+          this.selectedFiles = null;
+          this.$refs.fileupload.value = null;
         })
         .catch((err) => {
           console.log(err);
         });
     },
     onSelectedFile(event) {
-      this.selectedFiles = event.target.files[0];
+      if (event.target.files[0]["type"] === "application/pdf") {
+        if (event.target.files[0].size <= 16777216) {
+          this.selectedFiles = event.target.files[0];
+          this.fileErrors = null;
+        } else {
+          this.fileErrors = "Maximum size allowed is 2Mb";
+        }
+      } else {
+        this.fileErrors = "PDF file only allowed";
+      }
     },
   },
 };
@@ -52,14 +92,17 @@ export default {
       <div class="mb-6">
         <label for="mapel" class="block mb-2 text-sm font-medium text-white">Mata Pelajaran</label>
         <input type="text" v-model="mapel" id="mapel" name="mapel" class="text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 bg-gray-700 border-gray-600 placeholder-gray-400 text-white" />
+        <span class="text-red-500">{{ mapelError }}</span>
       </div>
       <div class="mb-6">
         <label for="kelas" class="block mb-2 text-sm font-medium text-white">Kelas</label>
         <input type="text" id="kelas" v-model="kelas" name="kelas" class="text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 bg-gray-700 border-gray-600 placeholder-gray-400 text-white" />
+        <span class="text-red-500">{{ kelasError }}</span>
       </div>
       <div class="mb-6">
         <label for="jurusan" class="block mb-2 text-sm font-medium text-white">Jurusan</label>
         <input type="text" id="jurusan" v-model="jurusan" name="jurusan" class="text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 bg-gray-700 border-gray-600 placeholder-gray-400 text-white" />
+        <span class="text-red-500">{{ jurusanError }}</span>
       </div>
       <div class="mb-6">
         <label class="block mb-2 text-sm font-medium text-white" for="file_input">Upload file</label>
@@ -69,9 +112,12 @@ export default {
           aria-describedby="file_input_help"
           id="file_input"
           type="file"
+          ref="fileupload"
           @change="onSelectedFile"
         />
-        <p class="mt-1 text-sm text-gray-300" id="file_input_help">SVG, PNG, JPG or GIF (MAX. 800x400px).</p>
+        <p class="mt-1 text-sm text-gray-300" id="file_input_help">
+          PDF only (max: 2Mb). <span class="text-red-500">{{ fileErrors }}</span>
+        </p>
       </div>
 
       <button type="submit" class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 mr-2 mb-2 focus:outline-none" @click="sendMapel">Simpan</button>
