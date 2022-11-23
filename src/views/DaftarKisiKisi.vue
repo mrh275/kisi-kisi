7c6ee9783495e2a1dd6b94183eec2435eccc34f6
Muhamad Ramdani Hidayatullah <muhamadramdani.h275@gmail.com>
2022-11-23 12:02:12 +0700

    fixes request for delete kisi-kisi and add alert for deleted kisi-kisi

--- src/views/DaftarKisiKisi.vue
+++ src/views/DaftarKisiKisi.vue
@@ -27,7 +27,7 @@ export default {
       ],
       items: [],
       searchValue: searchValue,
-      reloadTable: 0,
+      flashMessage: sessionStorage.flashMessage ? sessionStorage.flashMessage : null,
     };
   },
   methods: {
@@ -75,18 +75,16 @@ export default {
           },
         })
         .then((response) => {
-          console.log(response.data);
+          sessionStorage.setItem("flashMessage", response.data);
           window.location.href = "/daftar-kisi-kisi";
-          const toaster = createToaster({
-            position: "top-right",
-            duration: 3000,
-            dismissible: true,
-          });
-          setTimeout(toaster.success(response.data), 3000).then(this.$router.go("/daftar-kisi-kisi"));
         })
         .catch((err) => {
           console.log(err);
         });
+    },
+    removeAlert() {
+      sessionStorage.removeItem("flashMessage");
+      this.flashMessage = null;
     },
   },
 };
@@ -102,6 +100,32 @@ export default {
         Tahun Pelajaran 2022/2023
       </h2>
     </div>
+
+    <!-- Alert dismissing -->
+    <div id="alert-3" v-if="flashMessage != null" class="flex p-4 mb-4 bg-green-100 rounded-lg dark:bg-green-200" role="alert">
+      <svg aria-hidden="true" class="flex-shrink-0 w-5 h-5 text-green-700 dark:text-green-800" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
+        <path fill-rule="evenodd" d="M18 10a8 8 0 11-16 0 8 8 0 0116 0zm-7-4a1 1 0 11-2 0 1 1 0 012 0zM9 9a1 1 0 000 2v3a1 1 0 001 1h1a1 1 0 100-2v-3a1 1 0 00-1-1H9z" clip-rule="evenodd"></path>
+      </svg>
+      <span class="sr-only">Info</span>
+      <div class="ml-3 text-sm font-medium text-green-700 dark:text-green-800">{{ flashMessage }}</div>
+      <button
+        type="button"
+        class="ml-auto -mx-1.5 -my-1.5 bg-green-100 text-green-500 rounded-lg focus:ring-2 focus:ring-green-400 p-1.5 hover:bg-green-200 inline-flex h-8 w-8 dark:bg-green-200 dark:text-green-600 dark:hover:bg-green-300"
+        data-dismiss-target="#alert-3"
+        aria-label="Close"
+        @click="removeAlert()"
+      >
+        <span class="sr-only">Close</span>
+        <svg aria-hidden="true" class="w-5 h-5" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
+          <path
+            fill-rule="evenodd"
+            d="M4.293 4.293a1 1 0 011.414 0L10 8.586l4.293-4.293a1 1 0 111.414 1.414L11.414 10l4.293 4.293a1 1 0 01-1.414 1.414L10 11.414l-4.293 4.293a1 1 0 01-1.414-1.414L8.586 10 4.293 5.707a1 1 0 010-1.414z"
+            clip-rule="evenodd"
+          ></path>
+        </svg>
+      </button>
+    </div>
+    <!-- End of alert -->
 
     <!-- Table Kisi -->
     <div class="px-2 py-4 text-gray-800 bg-white rounded-lg xl:px-6 xl:py-6 table-wrapper">
