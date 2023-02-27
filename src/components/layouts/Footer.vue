a6fe84d7d2532b188d92fed9dda9a1b0f8322cf6
Muhamad Ramdani Hidayatullah <muhamadramdani.h275@gmail.com>
2023-02-27 13:42:00 +0700

    update version code

--- src/components/layouts/Footer.vue
+++ src/components/layouts/Footer.vue
@@ -11,6 +11,6 @@ export default {
 <template>
   <footer class="flex flex-col px-6 py-6 mb-20 text-center text-white bg-gray-700 xl:mb-0 footer">
     <span class="copyright">Copyright {{ dateNow }} ©️ SMAN 1 Rawamerta. All Right Reserved.</span>
-    <span>v1.1.2 Made by <a href="https://muhamadramdani.my.id" target="_blank" class="hover:text-[#0099ff] border-b border-dotted">dNezast</a>.</span>
+    <span>v1.2.5 Made by <a href="https://muhamadramdani.my.id" target="_blank" class="hover:text-[#0099ff] border-b border-dotted">dNezast</a>.</span>
   </footer>
 </template>
