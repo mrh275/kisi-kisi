b6998d18b6d3ab5bbf5b5d459ff00e62ea2f3702
Muhamad Ramdani Hidayatullah <muhamadramdani.h275@gmail.com>
2023-02-27 12:11:23 +0700

    initiate vue-sweetalert

--- src/main.js
+++ src/main.js
@@ -11,6 +11,8 @@ import Vue3EasyDataTable from "vue3-easy-data-table";
 import "vue3-easy-data-table/dist/style.css";
 import axios from "axios";
 import "flowbite";
+import VueSweetalert2 from 'vue-sweetalert2';
+import 'sweetalert2/dist/sweetalert2.min.css';
 
 axios.defaults.withCredentials = true;
 axios.defaults.baseURL = "https://api.sman1rawamerta.sch.id";
@@ -19,6 +21,7 @@ const app = createApp(App);
 
 app.use(createPinia());
 app.use(router);
+app.use(VueSweetalert2);
 app.component("fa", FontAwesomeIcon);
 app.component("EasyDataTable", Vue3EasyDataTable);
 
