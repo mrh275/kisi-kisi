8ac17232347cc3636b538cdf480436535f4d97a1
Muhamad Ramdani Hidayatullah <muhamadramdani.h275@gmail.com>
2024-03-11 01:02:59 +0700

    modified hero text

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -9,10 +9,10 @@ export default {
 <template>
   <main class="px-6 py-8 text-white bg-gray-800">
     <div class="first-wrapper xl:flex xl:justify-between xl:mb-6">
-      <div class="py-6 xl:py-12 greetings-wrapper xl:w-1/2">
+      <div class="py-6 xl:pl-12 xl:py-12 greetings-wrapper xl:w-1/2">
         <h1 class="text-4xl font-bold xl:text-5xl">Selamat Datang!</h1>
         <h2 class="text-3xl font-bold xl:text-4xl">di Portal Kisi-Kisi Ujian</h2>
-        <h3 class="text-2xl font-bold xl:text-3xl">Tahun Pelajaran 2022/2023</h3>
+        <h3 class="text-2xl font-bold xl:text-3xl">SMAN 1 Rawamerta</h3>
       </div>
       <div class="my-8 splash-img-wrapper xl:w-1/2 xl:flex xl:justify-center xl:items-center">
         <img src="../assets/study.svg" alt="Studying" class="xl:w-3/4" />
